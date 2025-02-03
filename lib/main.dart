1296a795d89e5d2eda09611daf013bbd05500566
muvaro2 <muvaro2@illinois.edu>
2025-02-03 11:28:27 -0600

    added support for themes. change the theme on line 16

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Wearable Device App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red) //**change this color to anything**
+      ),
       debugShowCheckedModeBanner: false,
       home: WearableDeviceHomepage(),
     );
@@ -22,10 +27,11 @@ class WearableDeviceHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       appBar: AppBar(
         title: const Text('Wearable Device for Sports Optimization and Injury Prevention'),
         centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: Center(
         child: Column(
@@ -33,7 +39,7 @@ class WearableDeviceHomepage extends StatelessWidget {
           children: [
             const Text(
               'Welcome to EOH Project Number {}!', // we can change what text is displayed here later
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(fontSize: 30),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 20), // Space between text and button
